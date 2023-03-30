24c3b61903dc996a957b3371a940351ba0308cc1
Abdelouakil BENTERKI <124548980+wb21CrackedSoul@users.noreply.github.com>
2023-03-30 11:12:27 +0100

    Update main.js
    the old script was made a year ago, summer 2022, when i started learning JS for the first time.
    this time i'm trying to update the code and solve the problems i encountered previously.

--- toDoList/main.js
+++ toDoList/main.js
@@ -1,5 +1,27 @@
-let taskInput = document.getElementById("task-input")
-let taskList = document.getElementById("task-list")
+let taskInput = document.getElementById('task-input')
+let taskListDOM = document.querySelector('#task-list')
+let taskList = document.querySelectorAll(".task-container")
+let taskCheckers = document.querySelectorAll(".task-container input")
+let taskListObj = []
+let currentCategory = 0
+
+taskInput.value = ''
+
+for (let i = 0; i < taskList.length; i++) {
+    taskListObj[i] = {
+        textValue: taskList[i].childNodes[3].innerText,
+        isChecked: taskCheckers[i].checked
+    }
+}
+
+taskCheckers = document.querySelectorAll(".task-container input")
+for (let i = 0; i < taskCheckers.length; i++) {
+    taskCheckers[i].addEventListener('click', function() {
+        taskListObj[i].isChecked = !taskListObj[i].isChecked
+        console.log(taskListObj[i].isChecked)
+    })
+}
+
 
 taskInput.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
@@ -9,64 +31,65 @@ taskInput.addEventListener("keydown", (e) => {
 })
 
 function addTask() {
-    let div = document.createElement("div")
-    let input = document.createElement("input")
-    let label = document.createElement("label")
-    if (taskInput.value == "") {
-        return false
-    }
-    else {
-        taskContainer = taskList.appendChild(div)
-        taskContainer.setAttribute("class", "task-container")
-        taskCheckbox = taskContainer.appendChild(input)
-        taskCheckbox.setAttribute("id", "")
-        taskCheckbox.setAttribute("type", "checkbox")
-        taskCheckbox.setAttribute("class", "task-checkbox")
-        taskCheckbox.checked = false
-        taskValue = taskContainer.appendChild(label)
-        taskValue.setAttribute("for", "")
-        taskValue.setAttribute("class", "task-value")
-        taskValue.innerHTML = taskInput.value
-        taskInput.value = ""
-    }
+    if (taskInput.value !== "") {
+        let newTaskObj = {
+            textValue: taskInput.value,
+            isChecked: false
+        }
+        taskListObj.push(newTaskObj)
+        taskInput.value = ''
+        updateDisplay(0)
+    } 
 }
 
 function clearTasks() {
-    const tasks = document.querySelectorAll(".task-container")
-    tasks.forEach(task => {
-        if (task.querySelector('input').checked === true) {
-            task.style.display = "none"
+    for (let i = 0; i < taskListObj.length; i++) {
+        if (taskListObj[i].isChecked) {
+            delete(taskListObj[i])
         }
-    });
+    }
+    taskListObj = taskListObj.filter(function(element) {
+        return element !== undefined;
+    }
+    )
+    updateDisplay(currentCategory)
 }
 
-function checkCategory(category) {
-    const tasks = document.querySelectorAll(".task-container")
-    
-    if (category === 'All') {
-        tasks.forEach(task => {
-            task.style.display = "flex"
-            }
-        );
+function updateDisplay(category) {
+    currentCategory = category
+    switchCategory()
+    taskListDOM.innerText = ""
+    taskList = []
+    for (let i = 0; i < taskListObj.length; i++) {
+        if (((category == 1) && (!taskListObj[i].isChecked)) ||
+            ((category == 2) && (taskListObj[i].isChecked)) ||
+            (category == 0))  {
+            let newTask = document.createElement('div')
+            newTask.setAttribute('class', 'task-container')
+            let newTaskInput = document.createElement('input')
+            newTaskInput.setAttribute('class', 'task-checkbox')
+            newTaskInput.setAttribute('type', 'checkbox')
+            // newTaskInput.setAttribute('onclick', 'switchChecked()')
+            newTaskInput.setAttribute('id', 'task' + i)
+            newTaskInput.checked = taskListObj[i].isChecked
+            let newTaskLabel = document.createElement('label')
+            newTaskLabel.setAttribute('class', 'task-value')
+            newTaskLabel.setAttribute('for', 'task' + i)
+            newTaskLabel.innerText = taskListObj[i].textValue
+            newTask.appendChild(newTaskInput)
+            newTask.appendChild(newTaskLabel)
+            taskList.push(newTask)
+        }
     }
-    else if (category === 'Pending') {
-        tasks.forEach(task => {
-            if (task.querySelector('input').checked === true) {
-                task.style.display = "none"
-            }
-            else if (task.querySelector('input').checked === false) {
-                task.style.display = 'flex'
-            }
-        });
+    for (let i = 0; i < taskList.length; i++) {
+        taskListDOM.appendChild(taskList[i])    
     }
-    else if (category === 'Completed') {
-        tasks.forEach(task => {
-            if (task.querySelector('input').checked === true) {
-                task.style.display = "flex"
-            }
-            else if (task.querySelector('input').checked === false) {
-                task.style.display = 'none'
-            }
-        });
+}
+
+function switchCategory() {
+    let categories = document.querySelectorAll('form input')
+    for (var i = 0; i < 3; i++) {
+        categories[i].checked = false
     }
+    categories[currentCategory].checked = true
 }
