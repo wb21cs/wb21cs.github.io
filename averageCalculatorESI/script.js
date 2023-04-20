a43a7d68b7290ee519c6c85b2c9202fd0dd8230b
Abdelouakil BENTERKI <124548980+wb21CrackedSoul@users.noreply.github.com>
2023-04-20 14:18:15 +0100

    Add files via upload

--- averageCalculatorESI/script.js
+++ averageCalculatorESI/script.js
@@ -1,20 +1,21 @@
 let inputFields = document.querySelectorAll('input')
 let inputArea = document.querySelectorAll('.input-area')
-let subjectAverageList = [] // this array stores the average value of each subject
+let semesterSelectors = document.querySelectorAll('.semester-selector')
+let subjectAverageList = [] /*this array stores the average value of each subject*/
 let rowLength = 4
 let chosenSemester = 0	/*it's self-descriptive, i declared it to
 						allow us access one of the subarrays
 						inside the 'semesters' array.
-						in the future updates, i'll make a selection
-						feature to choose which semester's
-						average should be calculated*/
+						the user will make a selection
+						to choose which semester's
+						average that should be calculated*/
 
 let semesters = [	/* this is an array containing
 					all necessary information about
 					the semesters in ESI-SBA
-					for now it only has 1CP-S1*/
+					for now it only has 1CP-S1 and 1CP-S2*/
 
-	[	// semeseter = S1 1CP
+	[	// 'index = 0' semeseter = S1 1CP
 		{
 			subjectAbr : 'ALSDS',
 			subjectFactor : 5,
@@ -55,28 +56,71 @@ let semesters = [	/* this is an array containing
 			subjectFactor : 1,
 			subjectComponents : [0, 1/3, 1/3, 1/3]
 		}
+	],
+	[	// 'index = 1' semeseter = S2 1CP
+		{
+			subjectAbr : 'ALSDD',
+			subjectFactor : 5,
+			subjectComponents : [2/9, 1/9, 1/3, 1/3]
+		},
+		{
+			subjectAbr : 'SYS2',
+			subjectFactor : 3,
+			subjectComponents : [0, 5/10, 2/10, 3/10]
+		},
+		{
+			subjectAbr : 'ANA2',
+			subjectFactor : 5,
+			subjectComponents : [1/3, 0, 1/3, 1/3]
+		},
+		{
+			subjectAbr : 'ALG2',
+			subjectFactor : 3,
+			subjectComponents : [1/3, 0, 1/3, 1/3]
+		},
+		{
+			subjectAbr : 'MECA',
+			subjectFactor : 3,
+			subjectComponents : [1/3, 0, 1/3, 1/3]
+		},
+		{
+			subjectAbr : 'ELECTRO',
+			subjectFactor : 4,
+			subjectComponents : [1/3, 0, 1/3, 1/3]
+		},
+		{
+			subjectAbr : 'TEO',
+			subjectFactor : 2,
+			subjectComponents : [0, 1/3, 1/3, 1/3]
+		},
+		{
+			subjectAbr : 'ANG1',
+			subjectFactor : 2,
+			subjectComponents : [0, 1/3, 1/3, 1/3]
+		}
 	]
 ]
 
 
 
 
+updateDisplay(chosenSemester)	/*this initiates the display*/
+updateAverage(chosenSemester) /* this updates the average once in case if the grid is already filled automatically*/
 
-updateAverage() /* this updates the average once in case if the
-				grid is already filled automatically*/
-
-inputFields.forEach((item) => {
-	item.addEventListener("input", updateAverage)
-	item.addEventListener("keypress", function(e) {
-		if (e.key === "Enter") {
-			e.preventDefault()
-			updateAverage()
-		}
+semesterSelectors.forEach((item) => {
+	item.addEventListener("click", () => {
+		chosenSemester = item.value
+		updateDisplay(chosenSemester)
+		console.log(inputFields)
 	})
-}) /*this allows the user to update the average after each input
-	change or an 'enter' key press*/
+})	/*this updates the display based on the semester selection of the user*/
 
-function updateAverage(e) {
+
+ 
+
+
+
+function updateAverage(chosenSemester) {
 
   	for (let i = 0; i < (inputArea.length / rowLength); i++) {
 		let subjectAverage = 0
@@ -88,7 +132,7 @@ function updateAverage(e) {
 			}
 		}
 		subjectAverageList[i] = parseFloat(subjectAverage)
-	} // this create an array containing the average of each subject
+	} /*this create an array containing the average of each subject*/
 
 
 	let totalAverage = 0
@@ -96,9 +140,53 @@ function updateAverage(e) {
 	for (let i = 0; i < subjectAverageList.length; i++) {
 		totalAverage += subjectAverageList[i]*semesters[chosenSemester][i].subjectFactor
 		sumFactors += semesters[chosenSemester][i].subjectFactor
-	} // this calculates the sum of all averages with their weight factor
+	} /*this calculates the sum of all averages with their weight factor*/
 
 	totalAverage = totalAverage / sumFactors
 	document.querySelector('.average-value').innerText = totalAverage.toFixed(2)
 }
 
+function updateDisplay(chosenSemester){
+	document.querySelector('.grid-container').innerHTML = `<div class="grid-element titles">MODULE</div>
+			<div class="grid-element titles">TD</div>
+			<div class="grid-element titles">TP</div>
+			<div class="grid-element titles">EMD1</div>
+			<div class="grid-element titles">EMD2</div>`
+
+	for (let i = 0; i < semesters[chosenSemester].length; i++) {
+		document.querySelector('.grid-container').innerHTML += `<div class="grid-element titles">${semesters[chosenSemester][i].subjectAbr}</div>`
+		for (let j = 0; j < rowLength; j++) {
+			if (semesters[chosenSemester][i].subjectComponents[j] === 0) {
+				document.querySelector('.grid-container').innerHTML += `<div class="grid-element titles blank input-area">-</div>`
+			}
+			else {
+				document.querySelector('.grid-container').innerHTML += `<div class="input input-area">
+				<input type="number" min="0" max="20" step="0.25" placeholder="0" inputmode="numeric" name="">
+				</div>`
+			}
+		}
+	}
+
+	document.querySelector('.grid-container').innerHTML += `<div class="average grid-element">Average</div>
+			<div class="average average-value grid-element">0.00</div>`
+
+
+
+	inputFields = document.querySelectorAll('input')
+	inputArea = document.querySelectorAll('.input-area')
+
+
+	inputFields.forEach((item) => {
+		item.addEventListener("input", function() {
+			updateAverage(chosenSemester)
+			console.log('i')
+		})
+		item.addEventListener("keypress", function(e) {
+			if (e.key === "Enter") {
+				e.preventDefault()
+				updateAverage(chosenSemester)
+				console.log('updatedk')
+			}
+		})
+	}) /*this allows the user to update the average after each input change or an 'enter' key press*/
+}
